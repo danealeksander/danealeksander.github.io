13d75b5e0ae26a7b9689db32f85892face46ce99
Dane <dane@lifeasplay.ca>
2018-12-18 11:04:17 -0400

    0.0.0.26
    - post-temp 5.0.1

--- ~unicorn (dark, light, rainbow)/twentythirteen/wp-includes-4.9.8/post-template.php
+++ ~unicorn (dark, light, rainbow)/twentythirteen/wp-includes-4.9.8/post-template.php
@@ -398,7 +398,11 @@ function get_the_excerpt( $post = null ) {
 }
 
 /**
- * Whether the post has a custom excerpt.
+ * Determines whether the post has a custom excerpt.
+ * 
+ * For more information on this and similar theme functions, check out
+ * the {@link https://developer.wordpress.org/themes/basics/conditional-tags/ 
+ * Conditional Tags} article in the Theme Developer Handbook.
  *
  * @since 2.3.0
  *
